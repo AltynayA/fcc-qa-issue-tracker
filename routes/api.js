fc566492a481344f5d97c48404deffc9989b32ed
altynay <goldmoon321123@gmail.com>
2025-06-09 21:14:03 +0500

    add post test

--- routes/api.js
+++ routes/api.js
@@ -1,36 +1,64 @@
 'use strict';
 
+var expect = require('chai').expect
+const mongoose = require('mongoose')
+let mongodb = require('mongodb')
+let uri = 'mongodb+srv://goldmoon321123:' +process.env.PW + '@xxxxxx.xxxxx.mongodb.net/<DATABASE>?retryWrites=true&w=majority'
 module.exports = function (app) {
+  module.exports = function (app) {
+    let uri = 'mongodb+srv://user1:' + process.env.PW + '@freecodecamp.b0myq.mongodb.net/issue_tracker?retryWrites=true&w=majority';
+    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
+    let issueSchema = new mongoose.Schema({
+      issue_title: {type: String, required: true},
+      issue_text: {type: String, required: true},
+      created_by: {type: String, required: true},
+      assigned_to: String,
+      status_text: String,
+      open: {type: Boolean, required: true},
+      created_on: {type: Date, required: true},
+      updated_on: {type: Date, required: true},
+      project: String
     })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      const {
-        issue_title,
-        issue_text,
-        created_by,
-        assigned_to = '',
-        status_text = '',
-      } = req.body;
-    })
-    if (!issue_title || !issue_text || !created_by) {
-      return res.status(400).json({ error: 'required field(s) missing' });
-    }
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
-    });
-    
-};
+    let Issue = mongoose.model('Issue', issueSchema);
+
+    app.route('/api/issues/:project')
+        .get(function (req, res) {
+          let project = req.params.project;
+
+        })
+        .post(function (req, res){
+          let project = req.params.project;
+          let newIssue = new Issue({
+            issue_title: req.body.issue_title,
+            issue_text: req.body.issue_text,
+            created_by: req.body.created_by,
+            assigned_to:req.body.assigned_to || '',
+            status_text: req.body.status_text || '',
+            open: true,
+            created_on: new Date().toUTCString(),
+            updated_on: new Date().toUTCString(),
+            project: project
+          })
+          newIssue.save((error, savedIssue) => {
+            if(!error && savedIssue) {
+              console.log(savedIssue)
+              // return
+            }
+          })
+
+        })
+
+
+        .put(function (req, res) {
+          let project = req.params.project;
+
+        })
+
+        .delete(function (req, res) {
+          let project = req.params.project;
+
+        });
+
+  };
+}
